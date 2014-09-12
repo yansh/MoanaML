20af20e352bf38215b6d79073e46412610a6bcc1
Yan Shvartzshnaider <yan.shvartzshnaider@cl.cam.ac.uk>
2014-09-12 14:11:53 +0100

    Added SQLlite implementation of the Store siganture. Still doesn't do much just testing waters.

--- main.ml
+++ main.ml
@@ -1,3 +1,5 @@
+open Printf
+  
 (* 
    List based storage
 *)
@@ -15,6 +17,54 @@ module LStore:Moana.STORE = struct
 
 end;;
 
+(* SQLlite based backend storage *)
+
+module SQLStore:Moana.STORE = struct
+  exception Dummy
+    
+  type t = Sqlite3.db
+    
+  let name = "Sql store"
+    
+  let init = 
+    let db = Sqlite3.db_open name in       
+      try
+        let sql =
+            sprintf "CREATE TABLE tbl (s varchar(10), p varchar(10), o varchar(10))"
+        in
+        (printf "%s\n%!" sql;
+         match Sqlite3.exec db sql ~cb: (fun _ _ -> print_endline "???") 
+         with
+           | Sqlite3.Rc.OK -> db
+           | _ -> assert false)    
+      with 
+        | xcp -> assert false
+        
+      
+     
+  let add db tuple =     
+      let x=print_endline "Adding fact" in 
+        let sql = sprintf "INSERT INTO tbl VALUES ('subject', 'predicate', 'object')" 
+        in
+          (printf " %s\n%!" sql;
+           try
+             match Sqlite3.exec db sql ~cb: (fun _ _ -> print_endline "???") with
+               | Sqlite3.Rc.OK -> (printf "Inserted %d rows\n%!" (Sqlite3.changes db); db)
+             | _ -> assert false
+           with 
+             | xcp -> (print_endline (Printexc.to_string xcp); db))
+          
+  let query (db:t) (query: Config.tuple list) =     
+     let sql = sprintf "SELECT * FROM tbl" in       
+         try
+           (print_endline "TESTING!";
+            match Sqlite3.exec db sql
+                    ~cb: (fun _ _ -> (print_endline "FOUND!"; raise Dummy))
+            with
+              | Sqlite3.Rc.OK -> (print_endline "OK"; query)
+            | _ -> assert false)
+         with | xcp -> (print_endline (Printexc.to_string xcp); query)       
+end;;  
 module G:Moana.GRAPH = struct    
   
   module LS = LStore
@@ -38,6 +88,8 @@ let t = Config.Tuple("subject", "predicate", "object", "context",None ,None);;
 let db = G.init;;
 G.add db t;;
 
+let sql_db = SQLStore.init;;
+SQLStore.add sql_db t;;
 (* generate Moana graph with a functor Make *)
 module MG = Moana.Make(LStore);;
 let db = MG.init;;
