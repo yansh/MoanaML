f6d3e54279bc16960de80d4c2974dfb765409ff3
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2014-09-18 14:19:00 +0100

    added debug flag;

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ocamlfind ocamlc -o moanaml -thread -linkpkg -package core,sqlite3 msqlite.ml config.ml moana.mli moana.ml main.ml
+ocamlfind ocamlc -g -o moanaml -thread -linkpkg -package core,sqlite3 msqlite.ml config.ml moana.mli moana.ml main.ml
