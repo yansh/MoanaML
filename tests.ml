6cddab13dcd0bd9b770b7d32d39d01b7261b5026
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-09-24 16:33:34 +0100

    Now includes and runs the unit tests

--- tests.ml
+++ tests.ml
@@ -1,6 +1,22 @@
-(* Compare the output results with expected results *)
+(*
+* Copyright (c) 2014 Yan Shvartzshnaider
+*
+* Permission to use, copy, modify, and distribute this software for any
+* purpose with or without fee is hereby granted, provided that the above
+* copyright notice and this permission notice appear in all copies.
+*
+* THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+* WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+* ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+* WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+* ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
 
 open Config
+(* Compare the output results with expected results *)
 
 let check_test l  l1  =
   let fl = List.flatten l and fl1 = List.flatten l1 in  
@@ -202,3 +218,40 @@ let q5 = {subj = Variable "?y";
           sign = None};;
 
 
+(****************** TESTS **********************************)
+
+   
+print_endline "---- Unit tests ----- ";;
+
+(* TEST 1:
+   
+ MAP {    
+     ?x, type, Car
+     ?x, hasColor, Red   
+    }
+     
+*)
+let test1 = (print_endline "Test 1") in 
+  let query1 = [q1;q2] and q1_exp_res = [[t1;t9]] in
+  let res_q1 = execute_query tuples query1 in check_test q1_exp_res res_q1;; 
+(*print_tuples_list res_q1;;
+ print_tuples_list q1_exp_res;;*)
+
+
+(* TEST 2:
+   
+ MAP  {    
+     ?x, type, Car
+     ?x, color, ?y
+     ?y, type, Color
+     ?y, rgbValue, White  
+    }
+     
+*)
+
+let test2 = (print_endline "Test 2") in 
+  let query2 = [q1;q3;q4;q5] and q2_exp_res = [[t1;t2;t5;t7]] in 
+let res_q2 = execute_query tuples query2 in check_test q2_exp_res res_q2 ;;
+
+(*print_tuples_list res_q2;;
+   print_tuples_list q2_exp_res;;*)
