4e786f27eb457303e158318ec52f17e482e95d6d
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-09-24 14:52:25 +0100

    Moved tuples and queries used for testing to test.ml

--- main.ml
+++ main.ml
@@ -17,6 +17,8 @@
 open Printf
 open Moana
 open Config   
+open Tests
+    
 (* 
    List based storage
 *)
@@ -197,132 +199,8 @@ let execute_query (db : tuple list) (qry : tuple list) : tuple list list =
     |> List.concat) qry [([], [])]
   |> List.map snd;; (*Map ext_tuple to tuple, since we don't need the valuation any longer*)
 
-(* Compare the output results with expected results *)
 
-let check_test l  l1  =
-  let fl = List.flatten l and fl1 = List.flatten l1 in  
-      let rec compare_list res exp_res = 
-        match res, exp_res with
-          [], [] -> true
-          | [], _
-          | _,  [] -> false
-          | h::t, h2::t2 ->  h=h2 && compare_list t t2  in         
-   let c = compare_list fl fl1  in                    
-      if c=true then 
-        print_endline "PASS"
-      else            
-        failwith "FAIL";;
     
-  
-
-let t1 = {subj = Constant "a";
-          pred = Constant "type";
-          obj = Constant  "Car";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-            
-
-let t2 = {subj = Constant "a";
-          pred = Constant "hasColor";
-          obj = Constant  "c";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-let t3 = {subj = Constant "b";
-          pred = Constant "type";
-          obj = Constant  "Chair";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-          
-let t4 = {subj = Constant "b";
-          pred = Constant "hasColor";
-          obj = Constant  "c";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-
-let t5 = {subj = Constant "c";
-          pred = Constant "type";
-          obj = Constant  "Color";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-let t6 = {subj = Constant "c1";
-          pred = Constant "type";
-          obj = Constant  "Color";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-          
-
-let t7 = {subj = Constant "c";
-          pred = Constant "rgbValue";
-          obj = Constant  "White";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-let t8 = {subj = Constant "c1";
-          pred = Constant "rgbValue";
-          obj = Constant  "Red";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-let t9 = {subj = Constant "a";
-          pred = Constant "hasColor";
-          obj = Constant  "Red";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-let tuples = [t1;t2;t3;t4;t5;t6;t7;t8;t9];;
-
-(*Config.print_tuples tuples;;*)
-
-
-(*************** QUERY TEMPLATE TUPLES *******************)
-
-let q1 = {subj = Variable "?x";
-          pred = Constant "type";
-          obj =  Constant "Car";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
-
-let q2 = {subj = Variable "?x";
-          pred = Constant "hasColor";
-          obj =  Constant "Red";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
-
-let q3 = {subj = Variable "?x";
-          pred = Constant "hasColor";
-          obj =  Variable "?y";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
-
-let q4 = {subj = Variable "?y";
-          pred = Constant "type";
-          obj =  Constant "Color";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
-
-let q5 = {subj = Variable "?y";
-          pred = Constant "rgbValue";
-          obj =  Constant "White";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
-
 
 
 print_endline "---- Unit tests----- ";;
@@ -359,7 +237,7 @@ check_test q1_exp_res res_q1;;
 
 print_endline "Running query 2, results ";;
 let query2 = [q1;q3;q4;q5] ;;
-let q2_exp_res = [[t1;t2;t5;t7];[t3;t4;t5;t7]];; 
+let q2_exp_res = [[t1;t2;t5;t7]];; 
 let res_q2 = execute_query tuples query2;;
 (*print_tuples_list res_q2;;
    print_tuples_list q2_exp_res;;*)
