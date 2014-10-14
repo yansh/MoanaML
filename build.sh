233447d9f78175cab8c99568b3302bf2f0e84fb4
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2014-10-15 00:28:53 +0100

    fixed dependency order;

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ocamlfind ocamlc -g -o moanaml -thread -linkpkg -package core,sqlite3 msqlite.ml \
-  config.ml moana_lists.ml tests.ml moana.mli moana.ml main.ml
+  config.ml moana.mli moana.ml moana_lists.ml tests.ml main.ml
