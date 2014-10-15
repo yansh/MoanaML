ec882b713ec3b94f879f65730959757d11a4f932
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-10-15 14:14:21 +0100

    * Added new tests to test functions, namely ** create AM ** mapping ** join ** filter

--- tests.ml
+++ tests.ml
@@ -1,254 +1,162 @@
-(*
-* Copyright (c) 2014 Yan Shvartzshnaider
-*
-* Permission to use, copy, modify, and distribute this software for any
-* purpose with or without fee is hereby granted, provided that the above
-* copyright notice and this permission notice appear in all copies.
-*
-* THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-* WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-* MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-* ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-* WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-* ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *)
-
+(* * Copyright (c) 2014 Yan Shvartzshnaider * * Permission to use, copy,   *)
+(* modify, and distribute this software for any * purpose with or without  *)
+(* fee is hereby granted, provided that the above * copyright notice and   *)
+(* this permission notice appear in all copies. * * THE SOFTWARE IS        *)
+(* PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES * WITH REGARD  *)
+(* TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF * MERCHANTABILITY  *)
+(* AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR * ANY SPECIAL,  *)
+(* DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES * WHATSOEVER  *)
+(* RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN * ACTION OF  *)
+(* CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF * OR IN   *)
+(* CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.                *)
 
 open Config
-  
+
 open OUnit;;
 (* Compare the output results with expected results *)
 
-(*let check_test l  l1  =
-  let fl = List.flatten l and fl1 = List.flatten l1 in  
-      let rec compare_list res exp_res = 
-        match res, exp_res with
-          [], [] -> true
-          | [], _
-          | _,  [] -> false
-          | h::t, h2::t2 ->  h=h2 && compare_list t t2  in         
-   let c = compare_list fl fl1  in                    
-      if c=true then 
-        print_endline "PASS"
-      else            
-        failwith "FAIL";; *)
+(* let check_test l l1 = let fl = List.flatten l and fl1 = List.flatten l1 *)
+(* in let rec compare_list res exp_res = match res, exp_res with [], [] -> *)
+(* true | [], _ | _, [] -> false | h::t, h2::t2 -> h=h2 && compare_list t  *)
+(* t2 in let c = compare_list fl fl1 in if c=true then print_endline       *)
+(* "PASS" else failwith "FAIL";;                                           *)
 
 (***************  TUPLES ***********************)
 
+let t1 = { subj = Constant "a";
+	pred = Constant "type";
+	obj = Constant "Car";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
+let t2 = { subj = Constant "a";
+	pred = Constant "hasColor";
+	obj = Constant "c";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let t1 = {subj = Constant "a";
-          pred = Constant "type";
-          obj = Constant  "Car";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-            
+let t3 = { subj = Constant "b";
+	pred = Constant "type";
+	obj = Constant "Chair";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let t2 = {subj = Constant "a";
-          pred = Constant "hasColor";
-          obj = Constant  "c";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
+let t4 = { subj = Constant "b";
+	pred = Constant "hasColor";
+	obj = Constant "c";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let t3 = {subj = Constant "b";
-          pred = Constant "type";
-          obj = Constant  "Chair";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-          
-let t4 = {subj = Constant "b";
-          pred = Constant "hasColor";
-          obj = Constant  "c";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
+let t5 = { subj = Constant "c";
+	pred = Constant "type";
+	obj = Constant "Color";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
+let t6 = { subj = Constant "c1";
+	pred = Constant "type";
+	obj = Constant "Color";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let t5 = {subj = Constant "c";
-          pred = Constant "type";
-          obj = Constant  "Color";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
+let t7 = { subj = Constant "c";
+	pred = Constant "rgbValue";
+	obj = Constant "White";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let t6 = {subj = Constant "c1";
-          pred = Constant "type";
-          obj = Constant  "Color";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-          
+let t8 = { subj = Constant "c1";
+	pred = Constant "rgbValue";
+	obj = Constant "Red";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let t7 = {subj = Constant "c";
-          pred = Constant "rgbValue";
-          obj = Constant  "White";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
+let t9 = { subj = Constant "a";
+	pred = Constant "hasColor";
+	obj = Constant "Red";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let t8 = {subj = Constant "c1";
-          pred = Constant "rgbValue";
-          obj = Constant  "Red";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
+let t10 = { subj = Constant "d";
+	pred = Constant "relatesTo";
+	obj = Constant "c";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let t9 = {subj = Constant "a";
-          pred = Constant "hasColor";
-          obj = Constant  "Red";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
+let t11 = { subj = Constant "c";
+	pred = Constant "relatesTo";
+	obj = Constant "d";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let tuples = [t1;t2;t3;t4;t5;t6;t7;t8;t9];;
+let tuples = [t1; t2; t3; t4; t5; t6; t7; t8; t9; t10; t11];;
 
-(*Config.print_tuples tuples;;*)
-
-
-(*************** QUERY TEMPLATE TUPLES *******************)
-
-let t1 = {subj = Constant "a";
-          pred = Constant "type";
-          obj = Constant  "Car";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-            
-
-let t2 = {subj = Constant "a";
-          pred = Constant "hasColor";
-          obj = Constant  "c";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-let t3 = {subj = Constant "b";
-          pred = Constant "type";
-          obj = Constant  "Chair";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-          
-let t4 = {subj = Constant "b";
-          pred = Constant "hasColor";
-          obj = Constant  "c";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-
-let t5 = {subj = Constant "c";
-          pred = Constant "type";
-          obj = Constant  "Color";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-let t6 = {subj = Constant "c1";
-          pred = Constant "type";
-          obj = Constant  "Color";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-          
-
-let t7 = {subj = Constant "c";
-          pred = Constant "rgbValue";
-          obj = Constant  "White";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-let t8 = {subj = Constant "c1";
-          pred = Constant "rgbValue";
-          obj = Constant  "Red";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-let t9 = {subj = Constant "a";
-          pred = Constant "hasColor";
-          obj = Constant  "Red";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-let t10 = {subj = Constant "d";
-          pred = Constant "relatesTo";
-          obj = Constant  "c";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-let t11 = {subj = Constant "c";
-          pred = Constant "relatesTo";
-          obj = Constant  "d";
-          ctxt = Constant "context";
-          time_stp =None;
-          sign = None};;
-
-
-let tuples = [t1;t2;t3;t4;t5;t6;t7;t8;t9;t10;t11];;
-
-(*Config.print_tuples tuples;;*)
-
+(* Config.print_tuples tuples;; *)
 
 (*************** QUERY TEMPLATE TUPLES *******************)
 
-let q1 = {subj = Variable "?x";
-          pred = Constant "type";
-          obj =  Constant "Car";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
+let q1 = { subj = Variable "?x";
+	pred = Constant "type";
+	obj = Constant "Car";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let q2 = {subj = Variable "?x";
-          pred = Constant "hasColor";
-          obj =  Constant "Red";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
+let q2 = { subj = Variable "?x";
+	pred = Constant "hasColor";
+	obj = Constant "Red";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let q3 = {subj = Variable "?x";
-          pred = Constant "hasColor";
-          obj =  Variable "?y";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
+let q3 = { subj = Variable "?x";
+	pred = Constant "hasColor";
+	obj = Variable "?y";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let q4 = {subj = Variable "?y";
-          pred = Constant "type";
-          obj =  Constant "Color";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
+let q4 = { subj = Variable "?y";
+	pred = Constant "type";
+	obj = Constant "Color";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let q5 = {subj = Variable "?y";
-          pred = Constant "rgbValue";
-          obj =  Constant "White";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
+let q5 = { subj = Variable "?y";
+	pred = Constant "rgbValue";
+	obj = Constant "White";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
+let q6 = { subj = Variable "?y";
+	pred = Constant "relatesTo";
+	obj = Variable "?x";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
 
-let q6 = {subj = Variable "?y";
-          pred = Constant "relatesTo";
-          obj =  Variable "?x";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
-          
-let q7 = {subj = Variable "?x";
-          pred = Constant "relatesTo";
-          obj =  Variable "?y";
-          ctxt = Constant "context";
-          time_stp = None;
-          sign = None};;
-          
+let q7 = { subj = Variable "?x";
+	pred = Constant "relatesTo";
+	obj = Variable "?y";
+	ctxt = Constant "context";
+	time_stp = None;
+	sign = None };;
+
+(****************** TESTS **********************************)
+
 (****************** TESTS **********************************)
 
    
@@ -328,7 +236,7 @@ let test4 _ =
  *)
 
 
-(* TEST  :
+(* TODO TEST  :
    
 MAP {
   ?X type GraduateStudent,
@@ -337,7 +245,7 @@ MAP {
 *)
 
 
-(* TEST :
+(* TODO TEST :
    
 MAP  {    
   X type GraduateStudent,
@@ -350,7 +258,7 @@ MAP  {
      
 *)
 
-(* TEST :
+(* TODO TEST :
    
 MAP  {    
   ?X type Student,
@@ -363,47 +271,43 @@ MAP  {
      
 *)
 
-let test5 _ = 
+(*  test filter function *)
+let test5 _ =
   let query5= q6 and q5_exp_res=[t10;t11] in
   let res_q5 = filter query5 tuples in assert_equal q5_exp_res res_q5 ;;
 
-
-(*  test filter function *)
-
-let test6 _ =
-  let query6= q6 and q6_exp_res=[t10;t11] in
-  let res_q6 = filter query6  tuples in assert_equal q6_exp_res res_q6 ;;
-
 (* create alpha memory and add tuples to it *)
-let test7 _ =
- let query7= q6 and q7_exp_res=[t10;t11] in
-  let am = create_alpha query7 [] in let new_am = add_to_alpha am tuples in assert_equal q7_exp_res new_am.tuples;;
+let test6 _ =
+    let query6 = q6 and q6_exp_res =[t10;t11] in
+    let am = create_am query6 tuples in (*let p = 
+		List.map (fun l -> match l with	  
+					| (var, values) -> print_string var; 
+					List.map (fun value -> match value with
+						| Constant x, t -> print_endline ""; print_string x;  print_endline (to_string t);
+						| Variable _, t-> print_string " ") values) am.vars in*) assert_equal q6_exp_res am.tuples;;
+
+(* test the join function *)		
+
+let test7 _ =     
+	let query7 = q6 and q7_exp_res ={solutions=[("?y",(Constant "a",[t1]));("?y",(Constant "a",[t2]));("?x",(Constant "Car",[t1]));("?x",(Constant "c",[t2]))]} in
+  let am = create_am query7 [t1;t2] in let bm = {solutions =[]} in 
+	let new_bm =join am bm in 
+    (*let p =List.map (fun l -> (* (string * (t element_type * tuple list) ) *)						
+		match l with
+		| var, (value, tuples) -> print_endline var; print_value value;
+              print_string "["; 
+                        List.map (fun t -> print_string (to_string t)) tuples )   new_bm.solutions  
+												in *)  assert_equal new_bm q7_exp_res;; 
 
 
-(* testing the mapping function *)
-let test8 _ =  
-  let query8= q6 and q8_exp_res=[("?y",1);("?x",3)] in
-  let m = mappings query8 in  assert_equal q8_exp_res  m;;
-(** for printing
- *     let p=List.map (fun t -> 
- *                match t with 
- *                 s, p -> 
- *                    (print_string s; print_string " "; print_int p;)
- *                  ) m   
- * *)  
-  
+let suite =
+	"Unit tests" >:::
+	[ "test1" >:: test1;
+	"test2" >:: test2;
+	"test3" >:: test3;
+	"test4" >:: test4;
+	"test5" >:: test5;
+	"test6" >:: test6;
+	"test7" >:: test7;]
 
-let suite = 
-  "Unit tests">::: 
-    [ "test1">:: test1;
-      "test2">:: test2;
-      "test3">:: test3;
-      "test4">:: test4;
-      "test5">:: test5;
-      "test6">:: test6;
-      "test7">:: test7;
-      "test8">:: test8]
-    
-let _= run_test_tt_main suite   
-  
-  
+let _ = run_test_tt_main suite
