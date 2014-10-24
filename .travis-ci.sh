3425658d893ea04c8cf685c2c5813ac0606629f3
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-10-24 13:54:58 +0100

    -- updated travis scripts for one OCaml environnment

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,16 +1,7 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS="ocamlfind ounit re core sqlite3"
 	 
-case "$OCAML_VERSION,$OPAM_VERSION" in
-3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
-esac
-	 
+ppa=avsm/ocaml41+opam11 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
