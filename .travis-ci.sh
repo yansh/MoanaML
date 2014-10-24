8d1685bfd956fed126c1f7eaf52da711d6041133
yansh <yansh@gmx.net>
2014-10-24 14:01:45 +0100

    Update .travis-ci.sh
    changed to OCaml 4.02.0 + OPAM 1.2.0

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,7 +1,7 @@
 # Edit this for your own project dependencies
 OPAM_DEPENDS="ocamlfind ounit re core sqlite3"
 	 
-ppa=avsm/ocaml41+opam11 
+ppa=avsm/ocaml42+opam12 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
