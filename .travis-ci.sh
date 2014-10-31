d54b46e5b8f6d7e2766aa460f78fa80840fe55ee
yansh <yansh@gmx.net>
2014-10-31 10:19:59 +0000

    Update .travis-ci.sh
    added irmin to the package list

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # Edit this for your own project dependencies
-OPAM_DEPENDS="ocamlfind ounit re core sqlite3-ocaml yojson lwt"
+OPAM_DEPENDS="ocamlfind ounit re core sqlite3-ocaml irmin yojson lwt"
 	 
 ppa=avsm/ocaml42+opam12 
 echo "yes" | sudo add-apt-repository ppa:$ppa
