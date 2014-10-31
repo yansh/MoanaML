69016fe408864f067c843c93d2563dbbe3d30f44
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-10-31 10:52:19 +0000

    - Added libgmp3-dev to fix compilation errors

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,7 +4,7 @@ OPAM_DEPENDS="ocamlfind ounit re core sqlite3-ocaml irmin yojson lwt"
 ppa=avsm/ocaml42+opam12 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra libgmp3-dev opam
 export OPAMYES=1
 opam init 
 opam install ${OPAM_DEPENDS}
