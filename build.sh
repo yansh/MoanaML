2322ee206be0a94f8bf1bfb790b467316f187266
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-10-31 09:31:26 +0000

    * Updated the build file to include lwt and irmin packages

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ocamlfind ocamlc -g -o moanaml -thread -linkpkg -package core,sqlite3 msqlite.ml \
-  config.ml moana.mli moana.ml moana_lists.ml tests.ml main.ml
+ocamlfind ocamlc -g -o moanaml -thread -linkpkg -package core,sqlite3,yojson,lwt,lwt.unix,irmin,irmin.unix msqlite.ml \
+  config.ml moana.mli moana.ml moana_lists.ml moana_irmin.ml tests.ml moana.ml main.ml
 
 ./moanaml
