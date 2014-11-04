a1e51d45fc3074938e068783e52b70930d53c056
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-11-04 11:42:54 +0000

    * cleaned the coded a bit

--- tests.ml
+++ tests.ml
@@ -10,300 +10,396 @@
 (* CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF * OR IN   *)
 (* CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.                *)
 open Config
-
+  
 open OUnit
 
 (***************  TUPLES ***********************)
 let t1 =
-	{
-		subj = Constant "a";
-		pred = Constant "type";
-		obj = Constant "Car";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Constant "a";
+    pred = Constant "type";
+    obj = Constant "Car";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let t2 =
-	{
-		subj = Constant "a";
-		pred = Constant "hasColor";
-		obj = Constant "c";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Constant "a";
+    pred = Constant "hasColor";
+    obj = Constant "c";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let t3 =
-	{
-		subj = Constant "b";
-		pred = Constant "type";
-		obj = Constant "Chair";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Constant "b";
+    pred = Constant "type";
+    obj = Constant "Chair";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let t4 =
-	{
-		subj = Constant "b";
-		pred = Constant "hasColor";
-		obj = Constant "c";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Constant "b";
+    pred = Constant "hasColor";
+    obj = Constant "c";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let t5 =
-	{
-		subj = Constant "c";
-		pred = Constant "type";
-		obj = Constant "Color";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Constant "c";
+    pred = Constant "type";
+    obj = Constant "Color";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let t6 =
-	{
-		subj = Constant "c1";
-		pred = Constant "type";
-		obj = Constant "Color";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Constant "c1";
+    pred = Constant "type";
+    obj = Constant "Color";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let t7 =
-	{
-		subj = Constant "c";
-		pred = Constant "rgbValue";
-		obj = Constant "White";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Constant "c";
+    pred = Constant "rgbValue";
+    obj = Constant "White";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let t8 =
-	{
-		subj = Constant "c1";
-		pred = Constant "rgbValue";
-		obj = Constant "Red";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Constant "c1";
+    pred = Constant "rgbValue";
+    obj = Constant "Red";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let t9 =
-	{
-		subj = Constant "a";
-		pred = Constant "hasColor";
-		obj = Constant "Red";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Constant "a";
+    pred = Constant "hasColor";
+    obj = Constant "Red";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let t10 =
-	{
-		subj = Constant "d";
-		pred = Constant "relatesTo";
-		obj = Constant "c";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Constant "d";
+    pred = Constant "relatesTo";
+    obj = Constant "c";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let t11 =
-	{
-		subj = Constant "c";
-		pred = Constant "relatesTo";
-		obj = Constant "d";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Constant "c";
+    pred = Constant "relatesTo";
+    obj = Constant "d";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let tuples = [ t1; t2; t3; t4; t5; t6; t7; t8; t9; t10; t11 ]
-
+  
 (* Config.print_tuples tuples;; *)
 (*************** QUERY TEMPLATE TUPLES *******************)
 let q1 =
-	{
-		subj = Variable "?x";
-		pred = Constant "type";
-		obj = Constant "Car";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Variable "?x";
+    pred = Constant "type";
+    obj = Constant "Car";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let q2 =
-	{
-		subj = Variable "?x";
-		pred = Constant "hasColor";
-		obj = Constant "Red";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Variable "?x";
+    pred = Constant "hasColor";
+    obj = Constant "Red";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let q3 =
-	{
-		subj = Variable "?x";
-		pred = Constant "hasColor";
-		obj = Variable "?y";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Variable "?x";
+    pred = Constant "hasColor";
+    obj = Variable "?y";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let q4 =
-	{
-		subj = Variable "?y";
-		pred = Constant "type";
-		obj = Constant "Color";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Variable "?y";
+    pred = Constant "type";
+    obj = Constant "Color";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let q5 =
-	{
-		subj = Variable "?y";
-		pred = Constant "rgbValue";
-		obj = Constant "White";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Variable "?y";
+    pred = Constant "rgbValue";
+    obj = Constant "White";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let q6 =
-	{
-		subj = Variable "?y";
-		pred = Constant "relatesTo";
-		obj = Variable "?x";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Variable "?y";
+    pred = Constant "relatesTo";
+    obj = Variable "?x";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 let q7 =
-	{
-		subj = Variable "?x";
-		pred = Constant "relatesTo";
-		obj = Variable "?y";
-		ctxt = Constant "context";
-		time_stp = None;
-		sign = None;
-	}
-
+  {
+    subj = Variable "?x";
+    pred = Constant "relatesTo";
+    obj = Variable "?y";
+    ctxt = Constant "context";
+    time_stp = None;
+    sign = None;
+  }
+  
 (****************** TESTS **********************************)
 let _ = print_endline "---- Unit tests ----- "
-
-(* TEST 1: MAP { ?x, type, Car ?x, hasColor, Red } *)
-let test1 _ =
-	let db = Moana_lists.S.init tuples in
-	let query1 = [ q1; q2 ] and q1_exp_res = [ [ t1; t9 ] ] in
-	let res_q1 = Moana_lists.S.query db query1
-	in assert_equal q1_exp_res res_q1
-
-(* print_tuples_list res_q1;; print_tuples_list q1_exp_res;; TEST 2: MAP { *)
-(* ?x, type, Car ?x, color, ?y ?y, type, Color ?y, rgbValue, White }test1  *)
-let test2 _ =
-	let query2 = [ q1; q3; q4; q5 ] and q2_exp_res = [ [ t1; t2; t5; t7 ] ] in
-	let res_q2 = execute_query tuples query2 in assert_equal q2_exp_res res_q2
-
-(* TEST 3: similar to test 1 query but the order of the tuple template is  *)
-(* different MAP { ?y, rgbValue, White ?x, type, Car ?x, color, ?y ?y,     *)
-(* type, Color }                                                           *)
-let test3 _ =
-	let query3 = [ q5; q1; q3; q4 ] and q3_exp_res = [ [ t7; t1; t2; t5 ] ] in
-	let res_q3 = execute_query tuples query3 in assert_equal q3_exp_res res_q3
-
-(* print_tuples_list res_q2;; print_tuples_list q2_exp_res;; TEST 4: -     *)
-(* Note - expected results are order sensetive MAP { ?x, relatesTo, ?y ?y, *)
-(* relatesTo, ?x }                                                         *)
-let test4 _ =
-	let query4 = [ q6; q7 ] and q4_exp_res = [ [ t11; t10 ]; [ t10; t11 ] ] in
-	let res_q4 = execute_query tuples query4 in assert_equal q4_exp_res res_q4
-
-(******* based on LUBM benchmark for SPARQL queries ****
-* http:// swat.cse.lehigh.edu / projects / lubm / queries - sparql.txt
+  
+(* TEST 1:
+   
+ MAP {    
+     ?x, type, Car
+     ?x, hasColor, Red   
+    }
+     
 *)
-(* TODO TEST : MAP { ?X type GraduateStudent, ?X takesCourse               *)
-(* GraduateCourse0} TODO TEST : MAP { X type GraduateStudent, ?Y type      *)
-(* University, ?Z type Department, ?X memberOf ?Z, ?Z subOrganizationOf    *)
-(* ?Y, ?X undergraduateDegreeFrom ?Y } TODO TEST : MAP { ?X type Student,  *)
-(* ?Y type Faculty, ?Z type Course, ?X advisor ?Y, ?Y teacherOf ?Z, ?X     *)
-(* takesCourse ?Z, } test filter function                                  *)
+let test1 _ =
+  let db = Moana_lists.S.init tuples in
+  let query1 = [ q1; q2 ] and q1_exp_res = [ [ t1; t9 ] ] in
+  let res_q1 = Moana_lists.S.query db query1
+  in assert_equal q1_exp_res res_q1
+  
+(*print_tuples_list res_q1;;
+ print_tuples_list q1_exp_res;;*)
+(* TEST 2:
+   
+ MAP  {    
+     ?x, type, Car
+     ?x, color, ?y
+     ?y, type, Color
+     ?y, rgbValue, White  
+    }test1
+     
+*)
+let test2 _ =
+  let query2 = [ q1; q3; q4; q5 ] and q2_exp_res = [ [ t1; t2; t5; t7 ] ] in
+  let res_q2 = execute_query tuples query2 in assert_equal q2_exp_res res_q2
+  
+(* TEST 3: similar to test 1 query but the order of the tuple template is different
+   
+ MAP  {    
+     ?y, rgbValue, White
+     ?x, type, Car
+     ?x, color, ?y
+     ?y, type, Color  
+    }
+     
+*)
+let test3 _ =
+  let query3 = [ q5; q1; q3; q4 ] and q3_exp_res = [ [ t7; t1; t2; t5 ] ] in
+  let res_q3 = execute_query tuples query3 in assert_equal q3_exp_res res_q3
+  
+(*print_tuples_list res_q2;;
+   print_tuples_list q2_exp_res;;*)
+(* TEST 4: - Note - expected results are order sensetive
+   
+ MAP  {    
+     ?x, relatesTo, ?y
+     ?y, relatesTo, ?x
+    }
+     
+*)
+let test4 _ =
+  let query4 = [ q6; q7 ] and q4_exp_res = [ [ t11; t10 ]; [ t10; t11 ] ] in
+  let res_q4 = execute_query tuples query4 in assert_equal q4_exp_res res_q4
+  
+(******* based on LUBM benchmark for SPARQL queries **** 
+ *  http://swat.cse.lehigh.edu/projects/lubm/queries-sparql.txt
+ *)
+(* TODO TEST  :
+   
+MAP {
+  ?X type GraduateStudent,
+  ?X takesCourse GraduateCourse0}
+     
+*)
+(* TODO TEST :
+   
+MAP  {    
+  X type GraduateStudent,
+  ?Y type University,
+  ?Z type Department,
+  ?X memberOf ?Z,
+  ?Z subOrganizationOf ?Y,
+  ?X undergraduateDegreeFrom ?Y 
+  }
+     
+*)
+(* TODO TEST :
+   
+MAP  {    
+  ?X type Student,
+  ?Y type Faculty,
+  ?Z type Course,
+  ?X advisor ?Y,
+  ?Y teacherOf ?Z,
+  ?X takesCourse ?Z,
+}
+     
+*)
+(*  test filter function *)
 let test5 _ =
-	let query5 = q6 and q5_exp_res = [ t10; t11 ] in
-	let res_q5 = filter query5 tuples in assert_equal q5_exp_res res_q5
-
+  let query5 = q6 and q5_exp_res = [ t10; t11 ] in
+  let res_q5 = filter query5 tuples in assert_equal q5_exp_res res_q5
+  
 (* create alpha memory and add tuples to it *)
 let test6 _ =
-	let query6 = q6 and q6_exp_res = [ t10; t11 ] in
-	let am = create_am query6 tuples
-	in assert_equal q6_exp_res am.tuples
-
+  let query6 = q6 and q6_exp_res = [ t10; t11 ] in
+  let am = create_am query6 tuples
+  in assert_equal q6_exp_res am.tuples
+  
 (* test simple join function AM with empty BM *)
 let test7 _ =
-	let q7_exp_res = { solutions = [ ("?x", ((Constant "a"), [ t1 ])) ]; } in
-	let am = create_am q1 tuples in
-	let bm = { solutions = []; } in
-	let new_bm = join am bm
-	in assert_equal new_bm q7_exp_res
-
-(* TEST : Implement by create a combination of AM and BM MAP { ?x, type,   *)
-(* Car ?x, hasColor, Red }                                                 *)
+  let q7_exp_res = { solutions = [ ("?x", ((Constant "a"), [ t1 ])) ]; } in
+  let am = create_am q1 tuples in
+  let bm = { solutions = []; } in
+  let new_bm = join am bm
+  in  assert_equal new_bm q7_exp_res
+  
+(* TEST :
+ 
+Implement by create a combination of AM and BM
+ MAP {    
+     ?x, type, Car
+     ?x, hasColor, Red   
+    }
+*)
 let test8 _ =
-	let am1 = create_am q1 tuples and am2 = create_am q2 tuples
-	
-	and q8_exp_res =
-		{ solutions = [ ("?x", ((Constant "a"), [ t9; t1 ])) ]; } in
-	let bm = { solutions = []; } in
-	let res_bm = join am2 (join am1 bm)
-	in assert_equal res_bm q8_exp_res
+  let am1 = create_am q1 tuples and am2 = create_am q2 tuples
 
-(* TEST 2: MAP { ?x, type, Car ?x, hasColor, ?y ?y, type, Color ?y,        *)
-(* rgbValue, White }                                                       *)
+  and q8_exp_res =
+    { solutions = [ ("?x", ((Constant "a"), [ t9; t1 ])) ]; } in
+  let bm = { solutions = []; } in
+  let res_bm = join am2 (join am1 bm)
+  in assert_equal res_bm q8_exp_res
+  
+(* TEST 9: 
+   
+ MAP  {    
+     ?x, type, Car
+     ?x, hasColor, ?y
+     ?y, type, Color
+     ?y, rgbValue, White  
+    }
+*)
 let test9 _ =
-	let q9_exp_res =
-		{ solutions = [ ("?y", ((Constant "c"), [ t7; t5; t2; t1 ])) ]; } in
-	let am1 = create_am q1 tuples and am2 = create_am q3 tuples
+  let q9_exp_res =
+    { solutions = [ ("?y", ((Constant "c"), [ t7; t5; t2; t1 ])) ]; } in
+  let am1 = create_am q1 tuples and am2 = create_am q3 tuples
+
+  and am3 = create_am q4 tuples and am4 = create_am q5 tuples in
+  (*let p = print_mappings am2*)
+  let res_bm = join am4 (join am3 (join am2 (join am1 { solutions = []; })))
+  in (*let p = print_bm res_bm in *) assert_equal res_bm q9_exp_res
 	
-	and am3 = create_am q4 tuples and am4 = create_am q5 tuples in
-	(* let p = print_mappings am2 *)
-	let res_bm = join am4 (join am3 (join am2 (join am1 { solutions = []; })))
-	in (*let p = print_bm res_bm in *) assert_equal res_bm q9_exp_res
 
-(* Tests for Irmin backed *)
+(* TEST 9: 
+   
+ MAP  {    
+     ?x, type, Car
+     ?x, hasColor, ?y
+     ?y, type, Color
+     ?y, rgbValue, White  
+    }
+*)
+let test9' _ =
+  let q9'_exp_res =
+    { solutions = [ ("?y", ((Constant "c"), [ t7; t5; t2; t1 ])) ]; } in
+  let am1 = create_am q1 tuples and am2 = create_am q3 tuples
 
-(* TEST 10: MAP { ?x, type, Car ?x, hasColor, Red } *)
+  and am3 = create_am q4 tuples and am4 = create_am q5 tuples in let am_list = [am1;am2;am3;am4] in
+  (*let p = print_mappings am2*)
+  let res_bm = execute_am_list (List.rev am_list)
+  in (*let p = print_bm res_bm in *) assert_equal res_bm q9'_exp_res
+	
+(* Tests for Irmin backed*)
+
+(* TEST 10:
+   
+ MAP {    
+     ?x, type, Car
+     ?x, hasColor, Red   
+    }
+     
+*)
 let test10 _ =
-	let db = Moana_irmin.S.init tuples in
-	let query10 = [ q1; q2 ] and q10_exp_res = [ [ t1; t9 ] ] in
-	let res_q10 = Moana_irmin.S.query db query10 in
-	(* let p = print_tuples_list res_q10 in let p1 = print_tuples_list       *)
-	(* q10_exp_resin                                                         *)
-	assert_equal q10_exp_res res_q10
+  let db = Moana_irmin.S.init tuples in
+  let query10 = [ q1; q2 ] and q10_exp_res = [ [ t1; t9 ] ] in
+  let res_q10 = Moana_irmin.S.query db query10 in 
+	(*let p = print_tuples_list res_q10 in let p1 = print_tuples_list q10_exp_resin*)
+	 assert_equal q10_exp_res res_q10	
 
-(* Test 11 Tests the add function and to_list function *)
+(* Test 11 *)
+(* Tests the add function and to_list function*)
 
 let test11 _ =
-	let db = Moana_irmin.S.init [t1; t2] in
-	let exp_res11 = [t1; t2; t3] in
-	let res11 = Moana_irmin.S.add db t3 in
-	(* let p = print_tuples_list res_q10 in let p1 = print_tuples_list     *)
-	(* q10_exp_resin                                                       *)
-	assert_equal exp_res11 (Moana_irmin.S.to_list res11)
-
+  let db = Moana_irmin.S.init [t1; t2] in
+  let exp_res11 = [t1; t2; t3] in
+  let res11 = Moana_irmin.S.add db t3 in     
+     assert_equal exp_res11 (Moana_irmin.S.to_list res11)   
+		
 let suite =
-	"Unit tests" >:::
-	[ "test1" >:: test1; "test2" >:: test2; "test3" >:: test3;
-	"test4" >:: test4; "test5" >:: test5; "test6" >:: test6;
-	"test7" >:: test7; "test8" >:: test8; "test9" >:: test9;
-	"test10" >:: test10; "test11" >:: test11]
-
+  "Unit tests" >:::
+    [ "test1" >:: test1; "test2" >:: test2; "test3" >:: test3;
+      "test4" >:: test4; "test5" >:: test5; "test6" >:: test6;
+      "test7" >:: test7; "test8" >:: test8; "test9" >:: test9; "test9'" >:: test9';  
+			"test10">:: test10; "test11">:: test11]
+  
 let _ = run_test_tt_main suite
+  
