83b72120d5e0c14ab333f3868ebbeae5fafa6748
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-11-04 11:37:20 +0000

    * added eval `opam config -env` to the build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+eval `opam config -env`
 ocamlfind ocamlc -g -o moanaml -thread -linkpkg \
   -package core,yojson,lwt,lwt.unix,irmin,irmin.unix,sqlite3 \
   msqlite.ml config.ml moana.mli moana.ml moana_lists.ml moana_irmin.ml \
