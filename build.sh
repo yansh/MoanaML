bb0cc205fe3100aca6290588577478aab6bd0f35
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-11-04 11:41:14 +0000

    * removed main.ml from the script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ eval `opam config -env`
 ocamlfind ocamlc -g -o moanaml -thread -linkpkg \
   -package core,yojson,lwt,lwt.unix,irmin,irmin.unix,sqlite3 \
   msqlite.ml config.ml moana.mli moana.ml moana_lists.ml moana_irmin.ml \
-  tests.ml main.ml
+  tests.ml
 
 ./moanaml
