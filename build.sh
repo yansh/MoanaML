89210c50072b0060010a4683a53d5a7f208afbd6
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2014-11-05 02:55:27 +0000

    fixed: incorrect argument was being given to opam config;

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-eval `opam config -env`
+
+eval `opam config env`
+
 ocamlfind ocamlc -g -o moanaml -thread -linkpkg \
   -package core,yojson,lwt,lwt.unix,irmin,irmin.unix,sqlite3 \
   msqlite.ml config.ml moana.mli moana.ml moana_lists.ml moana_irmin.ml \
