d0d23251a312c5ec532243f2a0a1fa4c38b0e69b
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-11-11 11:00:55 +0000

    - added rete.ml

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ eval `opam config env`
 
 ocamlfind ocamlc -g -o moanaml -thread -linkpkg \
   -package core,yojson,lwt,lwt.unix,irmin,irmin.unix,sqlite3 \
-  msqlite.ml config.ml moana.mli moana.ml moana_lists.ml moana_irmin.ml \
+  msqlite.ml config.ml moana.mli moana.ml moana_lists.ml moana_irmin.ml rete.ml \
   tests.ml
 
 ./moanaml
