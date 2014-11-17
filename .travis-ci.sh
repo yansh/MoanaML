7fdef198c4802d5f66e91285c834238b726d0c43
yansh <yansh@gmx.net>
2014-11-17 09:35:11 +0000

    Update .travis-ci.sh
    Added Menhir to installed packages

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # Edit this for your own project dependencies
-OPAM_DEPENDS="ocamlfind ounit re core sqlite3-ocaml yojson lwt irmin "
+OPAM_DEPENDS="ocamlfind ounit re core sqlite3-ocaml menhir yojson lwt irmin "
 	 
 ppa=avsm/ocaml42+opam12 
 echo "yes" | sudo add-apt-repository ppa:$ppa
