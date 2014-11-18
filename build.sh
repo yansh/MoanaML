6f79d706b8e1512a92c95b7c2c0cbe34900a5aa2
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-11-18 15:52:47 +0000

    * Added contacts.bytes to the build.sh file

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 eval `opam config env`
 
-ocamlbuild -use-ocamlfind -use-menhir -classic-display -package yojson,sqlite3,irmin,irmin.backend -libs unix,lwt-unix,git-unix,irmin-server,irmin-unix,irmin,sqlite3 -tag thread  tests.byte
+ocamlbuild -use-ocamlfind -use-menhir -classic-display -package yojson,sqlite3,irmin,irmin.backend -libs unix,lwt-unix,git-unix,irmin-server,irmin-unix,irmin,sqlite3 -tag thread  tests.byte contacts.byte
 
 ./tests.byte
+./contacts.byte
 
