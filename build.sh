b19987a539200a77ea4703d61b4412b972baa1e6
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-11-19 11:06:27 +0000

    Travis, why wouldn't you work - added irmin.unix to to build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 eval `opam config env`
 
-ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package yojson,sqlite3,irmin,irmin.backend -libs unix,lwt-unix,git-unix,irmin-server,irmin-unix,irmin -tag thread  tests.byte contacts.byte
+ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package yojson,sqlite3,irmin,irmin.backend,irmin.unix -libs unix,lwt-unix,git-unix,irmin-server,irmin-unix,irmin -tag thread  tests.byte contacts.byte
 
 ./tests.byte
 ./contacts.byte
