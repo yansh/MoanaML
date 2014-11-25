21dd8517f6bc663a3dde5e5ba0d072cb5bf5c3db
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-11-25 09:20:04 +0000

    - reordering the packages, hopefully will make Travis happy

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 eval `opam config env`
 
-ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package yojson,irmin,irmin.backend,irmin.unix -libs unix,lwt-unix,git-unix,irmin-server,irmin-unix,irmin -tag thread  tests.byte contacts.byte
+ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package yojson,irmin,irmin.backend,irmin.unix -libs unix,lwt-unix,git-unix,irmin,irmin-server,irmin-unix -tag thread  tests.byte contacts.byte
 
 
 ./tests.byte
