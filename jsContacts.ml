b61b67a763b0c980db41613ea1710a3296375831
Yan <yan.shvartzshnaider@cl.cam.ac.uk>
2014-12-05 14:46:00 +0000

    * added contact_to_json function

--- jsContacts.ml
+++ jsContacts.ml
@@ -45,11 +45,51 @@ let onmessage event =
 
 let _ = Js.Unsafe.set (Js.Unsafe.variable "self") (Js.string "onmessage") onmessage
 
+let contact_to_json contacts =
+  let q2 =
+    "MAP {
+        ?a,fn,?fn,contacts
+    ?a,last,?last,contacts    
+    ?a,email,?email,contacts
+    ?a,twitter,?tw,contacts
+    ?a,mobile,?mobile,contacts
+    ?a,title,?title,contacts 
+    ?a,image,?image,contacts
+    ?a,knows,?friend,contacts 
+        }" in
+  let v_to_string = function | Config.Constant x -> x | _ -> "" in
+  let get_field f map =
+    try List.hd (List.map v_to_string (Helper.StringMap.find f map))
+    with | Not_found -> "" in
+  let get_field_list f map =
+    try List.map v_to_string (Helper.StringMap.find f map)
+    with | Not_found -> [] in
+  let to_json id c =
+    let r = Rete.exec_qry q2 c in
+    let c_map =
+      Rete.get_res_map r
+        [ "?fn"; "?last"; "?email"; "?tw"; "?mobile"; "?title"; "?image";
+          "?friend"; "?work" ]
+    in
+      let open Contact_t
+      in
+        ({
+           id = id;
+           fn = get_field "?fn" c_map;
+           last = get_field "?last" c_map;
+           email = get_field "?email" c_map;
+           title = Some (get_field "?title" c_map);
+           image = Some (get_field "?image" c_map);
+           twitterId = Some (get_field "?tw" c_map);
+           work = Some (get_field "?work" c_map);
+           knows = Some (get_field_list "?friend" c_map);
+         } |> Contact_j.string_of_contact) |> Yojson.Basic.from_string
+  in List.mapi to_json contacts
 
-let results = Rete.exec_qry q1 (policies :: contacts) |> Rete.exec_bm q2    
-let Rete.Node (_, res_bm, _) = results  
-let res_str = List.fold_right (fun tpl acc -> (Helper.to_string tpl)^"\n"^acc)  (Helper.TupleSet.elements (Rete.get_tuples results)) "" 
-let js_query query = Js.string(res_str)
-                     
+(* return to page *) 
+let js_query txt  =
+		let contacts = Helper.to_tuple_collection (Js.to_string txt) in
+    let json = `List(contact_to_json contacts)in let pp =Yojson.Basic.pretty_to_string json in Js.string pp  
+ 	                     
              
 let _ = Js.Unsafe.set js_handler (Js.string "runQuery") (Js.wrap_callback js_query)
