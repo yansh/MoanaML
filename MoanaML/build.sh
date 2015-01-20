00292e249033460bb1cb5a7c97aaf7f15dab335e
Yan <yansh@gmx.net>
2015-01-20 10:39:23 -0500

    Fixing wrong commit by removing the duplicated MoanaML library

--- MoanaML/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-eval `opam config env`
-
-ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package yojson,atdgen,irmin,irmin.backend,irmin.unix -libs unix,lwt-unix,git-unix,irmin,irmin-server,irmin-unix -tag thread  tests.byte contacts.byte
-
-
-./tests.byte
-./contacts.byte
