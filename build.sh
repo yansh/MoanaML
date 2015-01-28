d32f588ea1eb11ba30f5669fb56d02987ebaa13a
Yan <yansh@gmx.net>
2015-01-28 18:35:24 -0500

    - update the build.sh removed uneccessary for the new version of irmin packages

--- build.sh
+++ build.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-#
-# Author: Carlos Molina-Jimenez, 6 Jan 2015, Computer Lab. Univ. of Cambridge
-#
-# This build.sh uses only findlib compliant libraries. It compiles
-# correctly yansh/MoanaML.
-#
-
 eval `opam config env`
 
 #note: currently this only works with Irmin 0.8.3
-ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package lwt,yojson,atdgen,irmin,irmin.backend,irmin.server,git.unix,irmin.unix -tag thread  tests.byte contacts.byte
+ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package lwt,yojson,irmin,git.unix,irmin.unix -tag thread  tests.byte contacts.byte irmintest.byte
 
 
 ./tests.byte
