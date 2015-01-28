c6139b2c6c3ad39b9e4506f4e95839621032650a
Yan <yansh@gmx.net>
2015-01-28 18:36:06 -0500

    - updated travis script to install the new version of irmin

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # Edit this for your own project dependencies
-OPAM_DEPENDS="ocamlfind ounit re core menhir yojson atdgen lwt irmin.0.8.3"
+OPAM_DEPENDS="ocamlfind ounit re core menhir yojson atdgen lwt irmin"
 	 
 ppa=avsm/ocaml42+opam12 
 echo "yes" | sudo add-apt-repository ppa:$ppa
