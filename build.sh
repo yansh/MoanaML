0d399880faca95c62de712d935cdfb67a9681564
yansh <yansh@gmx.net>
2015-01-29 12:57:20 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 eval `opam config env`
 
 #note: currently this only works with Irmin 0.8.3
-ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package lwt,yojson,irmin,irmin.unix -tag thread  tests.byte contacts.byte irmintest.byte
+ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package lwt,yojson,irmin,irmin.unix,oUnit -tag thread  tests.byte contacts.byte irmintest.byte
 
 
 ./tests.byte
