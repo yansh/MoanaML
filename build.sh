e943e1e1a72bc4ac9d8c46c92d7c18f3d0e870c4
Yan <yansh@gmx.net>
2015-02-02 10:48:13 -0500

    - update build.sh to support the new folder structure i.e., examples folder

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 
 eval `opam config env`
 
-#note: currently this only works with Irmin 0.8.3
-ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package lwt,yojson,irmin,irmin.unix,oUnit -tag thread  tests.byte contacts.byte
+ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package lwt,yojson,irmin,irmin.unix,oUnit -tag thread  tests.byte examples/contacts.byte
 
 
 ./tests.byte
