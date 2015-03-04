b2c841cd9b5693838f27d2778851284a84f31c52
Yan <yansh@gmx.net>
2015-03-04 17:57:07 -0500

    - added atdgen package

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ eval `opam config env`
 
 rm *.byte
 
-ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package lwt,yojson,irmin,irmin.unix,oUnit -tag thread  tests.byte examples/contacts.byte -cflags -annot
+ocamlbuild -r -use-ocamlfind -use-menhir -classic-display -package lwt,yojson,irmin,irmin.unix,oUnit,atdgen -tag thread  tests.byte examples/contacts.byte -cflags -annot
 
 
 ./tests.byte
