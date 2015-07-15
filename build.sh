c9843c10a0624e1c06e185e3dd1e7d877270d0d7
yansh <yansh@gmx.net>
2015-07-15 14:38:53 -0400

    Update build.sh
    Trying to fix TRAVIS atdgen error

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ eval `opam config env`
 
 rm *.byte
 
+atdgen -j Rete/rete_node.atd                                                    
+atdgen -t Rete/rete_node.atd 
+
 ocamlbuild -r -Is Rete,examples -use-ocamlfind -use-menhir -classic-display -package lwt,yojson,irmin,irmin.unix,oUnit,atdgen -tag thread  tests.byte examples/contacts.byte -cflags ' -annot'
 
 
